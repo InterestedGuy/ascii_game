d8f9c584f6860a603701597e90a34ececc8c16ef
عبدالرحمن <firstfireman1@gmail.com>
2022-09-02 21:16:38 +0200

    started player movement overhaul

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,5 @@ fn main() {
         color: String::from("green"),
         icon: String::from("@")};
     
-    while player.health != 0 {
-        render(&player, &level);
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Err(_) => break,
-            Ok(_) => {
-                let output = player.move_player(input);
-                if output.2 != "ok" {
-                    println!("{}", output.2);
-                    continue;
-                }else{
-                    let direction = output.0.to_lowercase();
-                    let steps:usize = output.1;
-                    if direction == String::from("w") && player.can_move(&level, &direction, steps){
-                        player.position.1 -= steps;
-                    }else if direction == String::from("a") && player.can_move(&level, &direction, steps){
-                        player.position.0 -= steps;
-                    }else if direction == String::from("s") && player.can_move(&level, &direction, steps){
-                        player.position.1 += steps;
-                    }else if direction == String::from("d") && player.can_move(&level, &direction, steps){
-                        player.position.0 += steps;
-                    }
-                }
-            }
-        };
-        println!("Game over !!!");
-    }
+    while player.health != 0 {}
 }
