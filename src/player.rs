9466354753d51e69f9a97738d7c76f0446061ddd
عبدالرحمن <firstfireman1@gmail.com>
2022-09-02 22:14:53 +0200

    player movement overhaul done

--- src/player.rs
+++ src/player.rs
@@ -13,7 +13,21 @@ impl Player{
         print!("{}", self.icon.color(color));
     }
 
-    pub fn can_move(&self, level: &Vec<Vec<String>>, steps:usize) -> bool{
-        true
+    pub fn can_move(&self, level: &Vec<Vec<String>>, movement: (&str, usize)) -> bool{
+        let x = self.position.0;
+        let y = self.position.1;
+        let direction = movement.0;
+        let steps = movement.1;
+        let mut result = true;
+        if direction == "w" {
+            for i in 0..=steps{
+                if level[y - i][x] != " "{
+                    result = false;
+                    break;
+                }
+                result = true;
+            }
+        }
+        return result;
     }
 }
